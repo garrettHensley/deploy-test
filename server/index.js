deb1cc3a9e3048e65c8a71b8a77376549a7a5a84
Garrett Hensley <garrettmichaelhensley@gmail.com>
2023-02-04 16:07:35 -0500

    stuff

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const { PORT, mongoUri } = require('./.env')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const TodoListRoutes = require('./routes/api/todoList')
+const path = require('path')
+require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => console.log('MongoDB database connected...'))
+  .catch((e) => console.log(e))
+
+app.use('/api/todoList', TodoListRoutes)
+app.listen(process.env.PORT, () => console.log(`app listening at http://localhost:${process.env.PORT}`))
---
+++ server/routes/api/Todolist.js
@@ -0,0 +1,38 @@
+const { Router } = require('express')
+const Todo = require('../../models/Todo')
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const todoList = await Todo.find()
+    if(!todoList) throw new Error('no todolist found')
+    res.status(200).json(todoList)
+  } catch(error) {
+    res.status(500).json({ message: error.message })
+  }
+})
+
+router.post('/', async (req, res) => {
+  const newTodo = new Todo(req.body)
+  try {
+    const todo = await newTodo.save()
+    if(!todo) throw new Error('something went wrong saving')
+    res.status(200).json(todo)
+  } catch(error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const removed = await Todo.findByIdAndDelete(id)
+    if(!removed) throw Error('something went wrong')
+    res.status(200).json(removed)
+  } catch(error) {
+    res.status(500).json({ message: error.message})
+  }
+})
+
+module.exports = router
